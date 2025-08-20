dafa2944f8fb277ff30a43ac5dff26bbc4d01b30
Dae Dowdell <dow_dell2010@yahoo.com>
2025-08-20 16:02:43 -0400

    create comment route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router';
+import { Routes, Route, useNavigate } from 'react-router';
 
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
@@ -9,16 +9,30 @@ import Dashboard from './components/Dashboard/Dashboard';
 
 import HootList from './components/HootList/HootList';
 import HootDetails from './components/HootDetails/HootDetails';
+import HootForm from './components/HootForm/HootForm';
 
 import { UserContext } from './contexts/UserContext';
 import * as hootService from './services/hootService';
 
 const App = () => {
   const { user } = useContext(UserContext);
-
   const [hoots, setHoots] = useState([]);
+  const navigate = useNavigate();
+  // const deletedHoot = await hootService.deleteHoot(hootId);
 
- useEffect(() => {
+  const handleAddHoot = async (hootFormData) => {
+    const newHoot = await hootService.create(hootFormData);
+    setHoots([newHoot, ...hoots]);
+    navigate('/hoots');
+  };
+
+  const handleDeleteHoot = async (hootId) => {
+    const deletedHoot = await hootService.deleteHoot(hootId);
+    setHoots(hoots.filter((hoot) => hoot._id !== deletedHoot._id));
+    navigate('/hoots');
+  };
+
+  useEffect(() => {
     const fetchAllHoots = async () => {
       const hootsData = await hootService.index();
 
@@ -35,10 +49,9 @@ const App = () => {
         {user ? (
           <>
             <Route path='/hoots' element={<HootList hoots={hoots} />} />
-            <Route 
-              path='/hoots/:hootId'
-              element={<HootDetails />}
-            />
+            <Route path='/hoots/:hootId' element={<HootDetails />} />
+            <Route path='/hoots/new' element={<HootForm handleAddHoot={handleAddHoot} />} />
+            <Route path='/hoots/:hootId' element={<HootDetails handleDeleteHoot={handleDeleteHoot} />} />
           </>
         ) : (
           <>
--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,11 +1,19 @@
 import { useParams } from 'react-router';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import * as hootService from '../../services/hootService';
+import CommentForm from '../CommentForm/CommentForm';
+import { UserContext } from '../../contexts/UserContext';
 
-const HootDetails = () => {
+
+const HootDetails = (props) => {
   const { hootId } = useParams();
-  // console.log('hootId', hootId);
+  const {user} = useContext(UserContext);
   const [hoot, setHoot] = useState(null);
+
+  const handleAddComment = async (commentFormData) => {
+    const newComment = await hootService.createComment(hootId, commentFormData);
+    setHoot({ ...hoot, comments: [...hoot.comments, newComment] });
+  };
 
   useEffect(() => {
     const fetchHoot = async () => {
@@ -15,24 +23,28 @@ const HootDetails = () => {
     fetchHoot();
   }, [hootId]);
 
-  // Verify the hoot state is set correctly:
-  // console.log('hoot state:', hoot);
   if (!hoot) return <main>Loading...</main>;
- return (
+  return (
     <main>
       <section>
         <header>
           <p>{hoot.category.toUpperCase()}</p>
-          <h1>{hoot.title}</h1>
+          <h2>{hoot.title}</h2>
           <p>
             {`${hoot.author.username} posted on
             ${new Date(hoot.createdAt).toLocaleDateString()}`}
           </p>
+          {hoot.author._id === user._id && (
+            <>
+              <button onClick={() => props.handleDeleteHoot(hootId)}>Delete</button>
+            </>
+          )}
         </header>
         <p>{hoot.text}</p>
       </section>
       <section>
         <h2>Comments</h2>
+        <CommentForm handleAddComment={handleAddComment} />
         {!hoot.comments.length && <p>There are no comments.</p>}
 
         {hoot.comments.map((comment) => (
@@ -46,8 +58,7 @@ const HootDetails = () => {
             <p>{comment.text}</p>
           </article>
         ))}
-      </section>
-    </main>
+      </section></main>
   );
 };
 
